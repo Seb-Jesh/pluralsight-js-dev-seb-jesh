ffcf4a868e39fbf4f500b5c9fb65271bfa63c8e3
Sebastian Jeshurun <sebastian.israel.2015@gmail.com>
2019-07-29 20:12:17 +0200

    modifications and additions

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -1,28 +1,24 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
+import compression from 'compression';
 
 /*eslint-disable no-console*/
 
 const port = 3000;
 const app = express();
-const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+app.use(compression());
+app.use(express.static('dist'));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
 
 app.get('/users', function(req, res) {
   //Hard coding for simpilicity. Pretend this hits a real database
   res.json([
-    {"id": 1,"firstName":"Lucy","lastName":"Chisha","email":"lucy.chisha@gmail.com"},
+    {"id": 1,"firstName":"Deborah","lastName":"Chisha","email":"lucy.chisha@gmail.com"},
     {"id": 2,"firstName":"Ruth","lastName":"Chasha","email":"ruth.chasha@gmail.com"},
     {"id": 3,"firstName":"Mary","lastName":"Chewe","email":"mary.chewe@gmail.com"}
   ]);
